4c3b44f8de85ab2ae43bdea59fde9f4597d182a3
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-04-14 06:07:16 +0700

    Use lmGC (tigerstats) With (In) R Software

--- lmGC/lmGC.R
+++ lmGC/lmGC.R
@@ -5,7 +5,7 @@
 install.packages("remotes")
 remotes::install_github("homerhanumat/tigerstats")
 library("tigerstats")
-lmGC = read.csv("https://raw.githubusercontent.com/timbulwidodostp/lmGC/main/lmGC/lmGC.csv",sep = ";")
+lmGC = read.csv("https://raw.githubusercontent.com/timbulwidodostp/lmGC_/main/lmGC/lmGC.csv",sep = ";")
 # Estimation Linear Regression Use lmGC (tigerstats) With (In) R Software
 lmGC <- lmGC(Dependen ~ Independen, data = lmGC, graph = TRUE)
 lmGC
