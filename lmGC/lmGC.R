dad0518a857a16de2a3273dd0469a80ab2f2fb6e
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-04-14 06:04:03 +0700

    Use lmGC (tigerstats) With (In) R Software

---
+++ lmGC/lmGC.R
@@ -0,0 +1,16 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Linear Regression Use lmGC (tigerstats) With (In) R Software
+install.packages("remotes")
+remotes::install_github("homerhanumat/tigerstats")
+library("tigerstats")
+lmGC = read.csv("https://raw.githubusercontent.com/timbulwidodostp/lmGC/main/lmGC/lmGC.csv",sep = ";")
+# Estimation Linear Regression Use lmGC (tigerstats) With (In) R Software
+lmGC <- lmGC(Dependen ~ Independen, data = lmGC, graph = TRUE)
+lmGC
+# Linear Regression Use lmGC (tigerstats) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
